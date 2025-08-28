f19698bd1cc9ea34d7e71abcca5a91c2d5bc9f1a
James O'Shea <james@klim.eco>
2025-08-28 23:46:38 +0200

    fix up load

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./App.css";
-import { setupMapInteractionHandlers } from "./utils/Map";
+
 import { Routing } from "./components/Routing";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiamFtZXNvc2hlYTg5IiwiYSI6ImNtZWFhdHQ2eDBwN2kyd3NoaHMzMWZhaHkifQ.VL1Krfm7XmukDNIHCpZnfg";
 
 /** lng, lat */
 export type Coordinate = [number, number];
@@ -13,33 +16,30 @@ const INITIAL_CENTER: Coordinate = [13.404954, 52.520008];
 const INITIAL_ZOOM = 10.12;
 
 function App() {
-  const mapRef = useRef<mapboxgl.Map | null>(null);
   const mapContainerRef = useRef<HTMLDivElement | null>(null);
-  const [_, setCenter] = useState<Coordinate>(INITIAL_CENTER);
-  const [__, setZoom] = useState<number>(INITIAL_ZOOM);
+
+  const [map, setMap] = useState<mapboxgl.Map | null>(null);
+  const [mapLoaded, setMapLoaded] = useState<boolean>(false);
 
   useEffect(() => {
-    mapboxgl.accessToken =
-      "pk.eyJ1IjoiamFtZXNvc2hlYTg5IiwiYSI6ImNtZWFhdHQ2eDBwN2kyd3NoaHMzMWZhaHkifQ.VL1Krfm7XmukDNIHCpZnfg";
-    mapRef.current = new mapboxgl.Map({
+    const newMap = new mapboxgl.Map({
       container: mapContainerRef.current as HTMLElement,
       center: INITIAL_CENTER,
       zoom: INITIAL_ZOOM,
-      style: 'mapbox://styles/mapbox/outdoors-v12', // style URL
-    });
+      style: "mapbox://styles/mapbox/outdoors-v12", // style URL
+    })
+    newMap.on("load", () => setMapLoaded(true));
+
+    setMap(newMap)
 
     return () => {
-      mapRef?.current?.remove();
+      newMap.remove();
     };
-  }, []);
-
-  useEffect(() => {
-    setupMapInteractionHandlers(mapRef, setCenter, setZoom);
   }, []);
 
   return (
     <>
-      {mapRef.current && <Routing map={mapRef.current} />}
+      {map && mapLoaded && <Routing map={map} />}
       <div id="map-container" ref={mapContainerRef} />
     </>
   );
