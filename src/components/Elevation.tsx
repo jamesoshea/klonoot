43111f0721263779d578b43d78c2bf90c0d15014
James O'Shea <james@klim.eco>
2025-08-30 07:48:33 +0200

    elevation styling

--- src/components/Elevation.tsx
+++ src/components/Elevation.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import type { BrouterResponse } from "../types";
 
 const scale = (
@@ -14,6 +14,7 @@ const scale = (
 export const Elevation = ({ routeTrack }: { routeTrack: BrouterResponse }) => {
   const canvasContainerRef = useRef<HTMLDivElement | null>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [canvasWidth, setCanvasWidth] = useState<number>(0)
 
   const maxElevation = routeTrack.features[0]?.properties?.["messages"]
     .slice(1)
@@ -34,7 +35,7 @@ export const Elevation = ({ routeTrack }: { routeTrack: BrouterResponse }) => {
 
   const CANVAS_HEIGHT = 128;
 
-  useEffect(() => {
+  const drawElevationMap = useCallback(() => {
     const canvas = canvasRef.current;
     if (canvas?.getContext) {
       const ctx = canvas.getContext("2d");
@@ -43,12 +44,13 @@ export const Elevation = ({ routeTrack }: { routeTrack: BrouterResponse }) => {
         return;
       }
 
-      const canvasWidth = canvasContainerRef.current?.clientWidth ?? 0;
+      const currentCanvasWidth = canvasContainerRef.current?.clientWidth ?? 0;
+      setCanvasWidth(currentCanvasWidth)
 
       const scaleYWithParams = (pointElevation: number) =>
         scale(pointElevation, minElevation, maxElevation, 0, CANVAS_HEIGHT);
       const scaleXWithParams = (pointDistance: number) =>
-        scale(pointDistance, 0, trackLength, 0, canvasWidth);
+        scale(pointDistance, 0, trackLength, 0, currentCanvasWidth);
 
       const dots = routeTrack.features[0]?.properties?.["messages"]
         .slice(1)
@@ -77,22 +79,32 @@ export const Elevation = ({ routeTrack }: { routeTrack: BrouterResponse }) => {
         ctx.lineTo(dot.left, CANVAS_HEIGHT - dot.top);
       }
 
-      ctx.strokeStyle = "rgb(200 0 0)";
+      ctx.strokeStyle = "rgb(255 255 255)";
       ctx.stroke();
     }
-  }, [maxElevation, minElevation, routeTrack, trackLength]);
+  }, [canvasWidth, maxElevation, minElevation, routeTrack, trackLength])
+
+  useEffect(() => {
+    window.addEventListener("resize", drawElevationMap)
+
+    return () => {
+      window.removeEventListener("resize", drawElevationMap)
+    }
+  }, [drawElevationMap])
+
+  useEffect(drawElevationMap, [drawElevationMap]);
 
   return (
     <div className="elevation">
-      <div className="p-3 rounded-lg bg-base-content text-primary-content flex items-center gap-3 w-full h-full">
-        <div className="flex flex-col items-center justify-between text-xs opacity-60 h-full">
+      <div className="rounded-lg bg-base-content text-primary-content flex items-center gap-3 p-3 w-full h-full">
+        <div className="flex flex-col items-center justify-between text-xs opacity-60 min-h-[128px]">
           <span>{maxElevation}</span>
           <span>{minElevation}</span>
         </div>
         <div className="w-full" ref={canvasContainerRef}>
           <canvas
             height={CANVAS_HEIGHT}
-            width={canvasContainerRef.current?.clientWidth}
+            width={canvasWidth}
             ref={canvasRef}
           ></canvas>
         </div>
