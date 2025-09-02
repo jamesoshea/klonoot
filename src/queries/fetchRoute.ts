40416a7a07b3633dcec37e75c226aa3449279864
James O'Shea <james@klim.eco>
2025-09-02 15:06:10 +0200

    stuff for prod

--- src/queries/fetchRoute.ts
+++ src/queries/fetchRoute.ts
@@ -3,21 +3,20 @@ import axios from "axios";
 import type { Coordinate } from "../App";
 import type { BrouterResponse } from "../types";
 
-
 async function fetchRoute(
   format: "gpx",
   points: Coordinate[],
-  brouterProfile: string,
+  brouterProfile: string
 ): Promise<string | null>;
 async function fetchRoute(
   format: "geojson",
   points: Coordinate[],
-  brouterProfile: string,
+  brouterProfile: string
 ): Promise<BrouterResponse | null>;
 async function fetchRoute(
   format: "gpx" | "geojson",
   points: Coordinate[],
-  brouterProfile: string,
+  brouterProfile: string
 ) {
   if (points.length < 2) {
     return null;
@@ -25,11 +24,14 @@ async function fetchRoute(
 
   const formattedLngLats = points.map((point) => point.join(",")).join("|");
   const formattedQueryString = `lonlats=${formattedLngLats}&profile=${brouterProfile}&alternativeidx=0&format=${format}`;
-  const resp = await axios.get(
-    `http://localhost:17777/brouter?${formattedQueryString}`
-  );
+
+  const baseUrl = import.meta.env.PROD
+    ? "https:klonoot.org/api"
+    : "http://localhost:17777";
+
+  const resp = await axios.get(`${baseUrl}/brouter?${formattedQueryString}`);
 
   return resp.data;
 }
 
-export { fetchRoute }
+export { fetchRoute };
