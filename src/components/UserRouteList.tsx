d0f706ee15a2f53e4178041d3cd468c882a5c7c2
James O'Shea <james@klim.eco>
2025-09-04 20:58:48 +0200

    more UI

--- src/components/UserRouteList.tsx
+++ src/components/UserRouteList.tsx
@@ -1,8 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit, faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faEdit,
+  faPlus,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 import { useGetUserRoutes } from "../queries/useGetUserRoutes";
 import { useCreateNewRoute } from "../queries/useCreateNewRoute";
+import { useState } from "react";
 
 export const UserRouteList = () => {
   const { data: queryData } = useGetUserRoutes();
@@ -11,31 +17,78 @@ export const UserRouteList = () => {
     isPending: createNewUserRouteMutationIsPending,
   } = useCreateNewRoute();
 
+  const [mode, setMode] = useState<"ADD" | "EDIT">("ADD");
+
   const userRoutes = queryData?.data ?? [];
 
   const handleEditRouteName = () => {};
 
   return (
     <div className="flex justify-between items-center w-100">
-      <select defaultValue={userRoutes?.[0]?.id ?? null} className="select">
-        {userRoutes.map((userRoute) => (
-          <option value={userRoute.id}>{userRoute.name}</option>
-        ))}
-      </select>
+      {mode === "ADD" && (
+        <select defaultValue={userRoutes?.[0]?.id ?? null} className="select">
+          {userRoutes.map((userRoute) => (
+            <option value={userRoute.id}>{userRoute.name}</option>
+          ))}
+        </select>
+      )}
+      {mode === "EDIT" && (
+        <input
+          type="text"
+          className="input"
+          defaultValue={userRoutes[0].name}
+        />
+      )}
       <div className="flex justify-end gap-2">
-        <button
-          className="btn btn-circle w-8 h-8 btn-ghost"
-          disabled={createNewUserRouteMutationIsPending}
-          onClick={() => createNewUserRoute()}
-        >
-          <FontAwesomeIcon className="cursor-pointer" icon={faPlus} size="lg" />
-        </button>
-        <button
-          className="btn btn-circle w-8 h-8 btn-ghost"
-          onClick={handleEditRouteName}
-        >
-          <FontAwesomeIcon className="cursor-pointer" icon={faEdit} size="lg" />
-        </button>
+        {mode === "ADD" && (
+          <>
+            <button
+              className="btn btn-circle w-8 h-8 btn-ghost"
+              disabled={createNewUserRouteMutationIsPending}
+              onClick={() => createNewUserRoute()}
+            >
+              <FontAwesomeIcon
+                className="cursor-pointer"
+                icon={faPlus}
+                size="lg"
+              />
+            </button>
+            <button
+              className="btn btn-circle w-8 h-8 btn-ghost"
+              onClick={() => setMode("EDIT")}
+            >
+              <FontAwesomeIcon
+                className="cursor-pointer"
+                icon={faEdit}
+                size="lg"
+              />
+            </button>
+          </>
+        )}
+        {mode === "EDIT" && (
+          <>
+            <button
+              className="btn btn-circle w-8 h-8 btn-ghost"
+              onClick={handleEditRouteName}
+            >
+              <FontAwesomeIcon
+                className="cursor-pointer"
+                icon={faCheck}
+                size="lg"
+              />
+            </button>
+            <button
+              className="btn btn-circle w-8 h-8 btn-ghost"
+              onClick={() => setMode("ADD")}
+            >
+              <FontAwesomeIcon
+                className="cursor-pointer"
+                icon={faXmark}
+                size="lg"
+              />
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
