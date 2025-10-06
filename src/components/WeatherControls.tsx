0d6876e6ce1cd79cdd15987542d096be60113649
James O'Shea <22768409+jamesoshea@users.noreply.github.com>
2025-10-06 16:47:12 +0200

    MVP weather

--- src/components/WeatherControls.tsx
+++ src/components/WeatherControls.tsx
@@ -1,20 +1,6 @@
 import dayjs from "dayjs";
 import { useWeatherContext } from "../contexts/WeatherContext";
-
-const validateDate = (valueAsNumber: number) => {
-  if (valueAsNumber > dayjs().add(46, "hour").valueOf()) {
-    return false;
-  }
-
-  if (dayjs(valueAsNumber).startOf("hour").valueOf() !== dayjs(valueAsNumber).valueOf()) {
-    return false;
-  }
-  if (valueAsNumber < dayjs().valueOf()) {
-    return false;
-  }
-
-  return true;
-};
+import { getSubstring } from "../utils/weather";
 
 export const WeatherControls = () => {
   const { pace, setPace, startTime, setStartTime } = useWeatherContext();
@@ -23,26 +9,17 @@ export const WeatherControls = () => {
     <div className="weather bg-base-100 rounded-lg px-2 pb-2">
       <fieldset className="fieldset">
         <legend className="fieldset-legend">Start time</legend>
-        <datalist id="available-dates">
+        <select value={startTime} onChange={(e) => setStartTime(e.target.value)}>
           {Array(46)
             .fill(null)
             .map((_, index) => (
-              <option value={dayjs().add(index, "hours").valueOf()} />
+              <option
+                value={getSubstring(dayjs().startOf("hour").add(index, "hours").toISOString())}
+              >
+                {dayjs().startOf("hour").add(index, "hours").format("ddd HH:mm")}
+              </option>
             ))}
-        </datalist>
-        <input
-          type="datetime-local"
-          className="input"
-          list="available-dates"
-          min={dayjs().startOf("hour").valueOf()}
-          step={1000 * 60 * 60}
-          value={startTime}
-          onChange={(e) => {
-            if (validateDate(e.target.valueAsNumber)) {
-              setStartTime(e.target.value);
-            }
-          }}
-        />
+        </select>
       </fieldset>
       <fieldset className="fieldset">
         <legend className="fieldset-legend">Average pace (km/h)</legend>
