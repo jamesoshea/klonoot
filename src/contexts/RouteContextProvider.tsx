0471f8bce1ce970095dd6b8aa9297a1ae6404276
James O'Shea <22768409+jamesoshea@users.noreply.github.com>
2025-11-15 20:50:40 +0100

    finish import functionality

--- src/contexts/RouteContextProvider.tsx
+++ src/contexts/RouteContextProvider.tsx
@@ -1,7 +1,7 @@
 import { useState, type ReactNode } from "react";
 import { RouteContext, type ShowPOIContextType } from "./RouteContext";
 import { useGetUserRoutes } from "../queries/useGetUserRoutes";
-import type { BrouterResponse } from "../types";
+import type { BrouterResponse, Coordinate } from "../types";
 
 export const RouteContextProvider = ({ children }: { children: ReactNode }) => {
   const { data: userRoutes } = useGetUserRoutes();
@@ -10,6 +10,7 @@ export const RouteContextProvider = ({ children }: { children: ReactNode }) => {
   const [routeTrack, setRouteTrack] = useState<BrouterResponse | null>(null);
   const [selectedRouteId, setSelectedRouteId] = useState<string | null>(null);
   const [showPOIs, setShowPOIs] = useState<ShowPOIContextType>({ transit: false, water: false });
+  const [points, setPoints] = useState<Coordinate[]>([]);
 
   const selectedUserRoute = userRoutes.find((userRoute) => userRoute.id === selectedRouteId);
 
@@ -18,7 +19,9 @@ export const RouteContextProvider = ({ children }: { children: ReactNode }) => {
       value={{
         currentPointDistance,
         setCurrentPointDistance,
+        points,
         routeTrack,
+        setPoints,
         setRouteTrack,
         selectedRouteId,
         setSelectedRouteId,
